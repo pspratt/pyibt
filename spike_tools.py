4b770056a372ce5feafb2450e290aa4eea4b3423
Perry Spratt <perrywespratt@gmail.com>
2019-09-30 22:40:49 -0700

    Spike_tools update

--- spike_tools.py
+++ spike_tools.py
@@ -1,10 +1,17 @@
+'''
+Functions to support the analysis of action potentials
+All functions are in beta
+'''
+
+
+
 import matplotlib.pyplot as plt
 import pyibt
 import numpy as np
 
-def get_spike_properties(ibt,sweep,spike_start_idx):
+def spike_properties(ibt,spike_time):
     '''
-    Returns dictionary with spike properties at the spike_start_idx
+    Returns dictionary with spike properties of the spike that starts
     spike_Vm
     height
     peak
@@ -12,7 +19,7 @@ def get_spike_properties(ibt,sweep,spike_start_idx):
     peak_dVdt
     width
     '''
-    ibt.set_sweep(sweep)
+    spike_start_idx = spike_time * ibt.sweep_points_per_sec
 
     #find zero crossings of dVdt after spike dVdt_threshold
     dVdt = ibt.dVdt[spike_start_idx:]
@@ -35,32 +42,32 @@ def get_spike_properties(ibt,sweep,spike_start_idx):
     properties['peak_Vm'] = np.max(spike_Vm)
     properties['height'] = np.max(spike_Vm)-spike_Vm[0]
     properties['peak_dVdt'] = np.max(dVdt[:spike_end_idx])
+    properties['min_dVdt'] = np.min(dVdt[:spike_end_idx])
 
     half_pnts = find_zero_crossing(spike_Vm - (spike_Vm[0]+(np.max(spike_Vm)-spike_Vm[0])/2))
     properties['half_width'] = (half_pnts[1]-half_pnts[0])/ibt.sweep_points_per_sec*1000
     return properties
 
-def get_spikes(ibt, sweep_num, dVdt_thresh = 15, min_spike_len = 0.1):
+
+def get_spikes(ibt):
+    spike_times = get_spike_times(ibt)
+    spikes = []
+    if not empty(spike_times):
+        for spike_time in spike_times:
+            spike.append(spike_properties(ibt,spike_time))
+    return spikes
+
+def get_spike_times(ibt, dVdt_thresh = 15, min_spike_len = 0.1):
     '''
-    Returns list of spikes
-
-    ibt: ibt object
-    sweep_num: sweep to detect detect_spike
-    dVdt_thresh: dVdt threshold for determining spike initiation
-    min_spike_len: minimum time in ms that the sweep must be above dVdt_threshold to be considered a spike
+    Returns list of spike start times
     '''
-    if ibt.sweeps[sweep_num]["rec_mode"] != 1:
-        raise Exception('Sweep must be recorded in current clamp mode')
-    ibt.set_sweep(sweep_num)
-
-
     #determine where dVdt exceeds dVdt_thresh
     runs = group_consecutives(np.argwhere((ibt.dVdt>dVdt_thresh)).flatten())
-    spikes = []
+    spike_times = []
     for run in runs:
         if len(run) > np.ceil(ibt.sweep_points_per_sec/(1000/min_spike_len)):
-            spikes.append(get_spike_properties(ibt,sweep_num,run[0]))
-    return spikes
+            spike_times.append(run[0]/ibt.sweep_points_per_sec)
+    return spike_times
 
 def group_consecutives(vals, step=1):
     """Return list of consecutive lists of numbers from vals (number list)."""
@@ -73,7 +80,7 @@ def group_consecutives(vals, step=1):
         else:
             run = [v]
             result.append(run)
-        expect = v + 1
+        expect = v + step
     return result
 
 def find_zero_crossing(x):
@@ -84,41 +91,11 @@ def find_zero_crossing(x):
     zero_crossings = np.where(np.diff(np.signbit(x)))[0]
     return zero_crossings
 
-def plot_spike(spike, ax=[]):
+def plot_spike(spike_properties, ax=[]):
     if ax == []:
         ax = plt.gca()
 
-    ax.plot(spike['time']-spike['time'][0],spike['Vm'])
+    ax.plot(spike_properties['time']-spike_properties['time'][0],spike_properties['Vm'])
     ax.set_xlabel('Time (seconds)')
     ax.set_ylabel('Membrane Potential (mV)')
     return ax
-
-def FI_analysis(ibt, sweep_start, num_sweeps, stim_start = .05, stim_dur = .3, dVdt_thresh = 15,
-                min_spike_len = 0.1):
-
-    FI_data = {}
-    FI_data['amp'] = []
-    FI_data['spikes'] = []
-    FI_data['Vm'] = []
-
-    for sweep_num in range(sweep_start, sweep_start + num_sweeps):
-        ibt.set_sweep(sweep_num)
-        stim_start_idx = int(stim_start*ibt.sweep_points_per_sec)
-        stim_end_idx = int(stim_start_idx + stim_dur*ibt.sweep_points_per_sec)
-
-
-        FI_data['amp'].append(ibt.sweep_C[stim_start_idx])
-        FI_data['Vm'].append(ibt.sweep_Y[0])
-
-        #Find all spikes in the sweep
-        all_spikes = get_spikes(ibt, sweep_num,
-                                        dVdt_thresh = dVdt_thresh,
-                                        min_spike_len = min_spike_len)
-        if len(all_spikes) > 0:
-            valid_spikes = [spike for spike in all_spikes if spike > stim_start_idx and
-                            spike <= stim_end_idx]
-            FI_data['spikes'].append(len(valid_spikes))
-        else:
-            FI_data['spikes'].append(0)
-
-    return FI_data
