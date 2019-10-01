8659eb7c3364a22d75d4ad6a5ffd28dd4909454f
Perry Spratt <perrywespratt@gmail.com>
2019-09-30 22:42:33 -0700

    Removed ability to load select sweeps as it is no unnecessary for quickly load ibts

--- IBT.py
+++ IBT.py
@@ -12,10 +12,7 @@ time: list of sweep times in seconds from the start of the experiment
 sweep_list: list of sweep numbers in ibt files
 temp: list of sweep temperatures
 count: number of sweeps in ibt files
-Vm_epoch: list of two numbers specifying start and stop times for calculating  Vm (see below)
-amp_epoch: list of four numbers [baseline_start,base_end,amp_start,amp_end] specifying times for calculating amp (see below)
-slope_epoch: list of two numbers specifiying start and stop times for calculating  slope (see below)
-Rin_epoch: list of four numbers [baseline_start,base_end,Rin_start,Rin_end] specifying times for calculating Rin (see below)
+
 sweep_Y: numpy array of Vm or pA data for sweep specified by set_sweep
 sweep_X: numpy array of time points for sweep specified by set_sweep
 sweep_C: numpy array of Vm or pA commands for sweep specified by set_sweep
@@ -26,15 +23,16 @@ sweep_Y_label: x_axis label for sweep set by set_sweep
 sweep_C_label: y_axis label sweep command for sweep set by set_sweep
 
 Properties:
-Vm: list of Vm values for each sweep calcuated as the mean between Vm_epoch
-amp: list of values for each sweep calcuated as the difference between baseline and amp specified by amp_epoch
-slope: list of slope values for each sweep calculated as the rise/run between points specified by slope_epoch
-Rin_epoch: list of Rin values for each sweep calculated from the baseline and rin points specified by Rin_epoch
 dVdt: time derivated of sweep_Y specified by set_sweep
 
 Functions:
 set_sweep: intitalizes sweep values
 Various plotting functions
+
+To do:
+Write functions to:
+convert time to sweep indicies
+return subsweeps values without loading full sweep to quickly measure Vm, Rin for each sweep
 """
 import os
 import numpy as np
@@ -65,12 +63,8 @@ class IBT:
         self.exp_details = get_experiment_details(ibt_File_Path)
         self._sweep_headers = get_sweep_headers(ibt_File_Path)
 
-        #Check if specific sweeps were specified, parse headers of only those sweeps
-        if not sweeps:
-            self.sweeps = parse_sweep_headers(ibt_File_Path, self._sweep_headers)
-        else:
-            sweep_headers = [self._sweep_headers[sweep] for sweep in sweeps]
-            self.sweeps = parse_sweep_headers(ibt_File_Path, sweep_headers)
+        #parse sweep headers to get sweep metadata
+        self.sweeps = parse_sweep_headers(ibt_File_Path, self._sweep_headers)
 
         self.time = []
         self.sweep_list = []
@@ -80,121 +74,35 @@ class IBT:
             self.time.append(sweep['sweep_time'])
             self.sweep_list.append(sweep['sweep_num'])
             self.temp.append(sweep['temperature'])
-        # self.Vm_epoch = [0,0.001] #[start,stop]
-        # self.amp_epoch = [0.048,0.05,0.0558,0.0658]#[baseline_start,baseline_stop,amp_start,amp_end]
-        # self.slope_epoch = [0.0524,0.0544] #[start,stop]
-        # self.Rin_epoch = [0.5,0.55,0.635,0.645]#[baseline_start,baseline_stop,amp_start,amp_end]
+
         self.set_sweep(self.sweep_list[0])
 
     def set_sweep(self,sweep_num):
         #update sweep values
-        sweep_idx = self.sweep_list.index(sweep_num)
+
+
+        if sweep_num < 0:
+            raise Exception("sweep index must be greater or equal to 0")
+        elif sweep_num > self.sweep_list[-1]:
+            raise Exception("sweep index is out of bounds")
 
         self.sweep_Y, self.sweep_X, self.sweep_C = get_sweep_data(self.ibt_File_Path,
-                                                                  self.sweeps[sweep_idx])
-        self.sweep_temp = self.sweeps[sweep_idx]['temperature']
+                                                                  self.sweeps[sweep_num])
+        self.sweep_temp = self.sweeps[sweep_num]['temperature']
         self.sweep_num = sweep_num
-        self.sweep_points_per_sec = self.sweeps[sweep_idx]["sample_rate"]
+        self.sweep_points_per_sec = self.sweeps[sweep_num]["sample_rate"]
 
         #set axes labels
         self.sweep_X_label = "Time (seconds)"
-        if self.sweeps[sweep_idx]["rec_mode"] == 1: #voltage clamp
+        if self.sweeps[sweep_num]["rec_mode"] == 1: #voltage clamp
             self.sweep_Y_label = "Membrane Potential (mV)"
             self.sweep_C_label = "Applied Current (pA)"
-        elif self.sweeps[sweep_idx]["rec_mode"] == 2: #voltage clamp
+        elif self.sweeps[sweep_num]["rec_mode"] == 2: #voltage clamp
             self.sweep_Y_label = "Clamp Current (pA)"
             self.sweep_C_label = "Command Voltage (mV)"
         else:
             self.sweep_Y_label = "unknown"
             self.sweep_C_label = "unknown"
-
-    # @property
-    # def Vm(self):
-    #     if len(self.Vm_epoch) != 2:
-    #         raise Exception("Vm_epoch must be a list with two numbers")
-    #     elif self.Vm_epoch[0] < 0:
-    #         raise Exception("Vm_epoch[0] must be greater than zero")
-    #     self._Vm = []
-    #     for sweep in self.sweeps:
-    #         if self.Vm_epoch[1] > sweep["dataX"][-1]:
-    #             raise Exception("Vm_epoch[1] must be less than the length of the sweep")
-    #         start_idx = int(self.Vm_epoch[0]*sweep["sample_rate"])
-    #         end_idx = int(self.Vm_epoch[1]*sweep["sample_rate"])
-    #         self._Vm.append(np.mean(sweep["dataY"][start_idx:end_idx]))
-    #     return self._Vm
-    #
-    # @property
-    # def Rin(self): #Output values seem incorrect
-    #     if len(self.Rin_epoch) != 4:
-    #         raise Exception("Vm_epoch must be a list with four numbers")
-    #     elif self.Rin_epoch[0] < 0 or self.Rin_epoch[2] < 0:
-    #         raise Exception("Rin_epoch[0] and Rin_epoch[2] must be greater than zero")
-    #     self._Rin = []
-    #     for sweep in self.sweeps:
-    #         if self.Rin_epoch[1] > sweep["dataX"][-1] or self.Rin_epoch[3] > sweep["dataX"][-1]:
-    #             raise Exception("Rin_epoch[1] and Rin_epoch[3] must be less than the length of the sweep")
-    #
-    #         baseline_start_idx = int(self.Rin_epoch[0]*sweep["sample_rate"])
-    #         baseline_end_idx = int(self.Rin_epoch[1]*sweep["sample_rate"])
-    #         baseline_Y = np.mean(sweep["dataY"][baseline_start_idx:baseline_end_idx])
-    #         baseline_C = np.mean(sweep["dataC"][baseline_start_idx:baseline_end_idx])
-    #
-    #         amp_start_idx = int(self.Rin_epoch[2]*sweep["sample_rate"])
-    #         amp_end_idx = int(self.Rin_epoch[3]*sweep["sample_rate"])
-    #         amp_Y = np.mean(sweep["dataY"][amp_start_idx:amp_end_idx])
-    #         amp_C = np.mean(sweep["dataC"][amp_start_idx:amp_end_idx])
-    #
-    #         #Use ohms law: V=IR R=V/I
-    #         if amp_C-baseline_C == 0: #no command
-    #             self._Rin.append(np.nan)
-    #         else:
-    #             if sweep["rec_mode"] == 1: #current clamp R=Y/C
-    #                 self._Rin.append(np.abs((amp_Y-baseline_Y)/(amp_C-baseline_C))*(1e-3/1e-12)/1e6)
-    #             elif sweep["rec_mode"] == 2: #voltage clamp R=C/Y
-    #                 self._Rin.append(np.abs(((amp_C-baseline_C)/amp_Y-baseline_Y))*(1e-3/1e-12)/1e6)
-    #     return self._Rin
-    #
-    # @property
-    # def amp(self):
-    #     if len(self.amp_epoch) != 4:
-    #         raise Exception("Vm_epoch must be a list with four numbers")
-    #     elif self.amp_epoch[0] < 0 or self.amp_epoch[2] < 0:
-    #         raise Exception("Rin_epoch[0] and Rin_epoch[2] must be greater than zero")
-    #     self._amp = []
-    #     for sweep in self.sweeps:
-    #         if self.amp_epoch[1] > sweep["dataX"][-1] or self.Rin_epoch[3] > sweep["dataX"][-1]:
-    #             raise Exception("Rin_epoch[1] and Rin_epoch[3] must be less than the length of the sweep")
-    #
-    #         baseline_start_idx = int(self.amp_epoch[0]*sweep["sample_rate"])
-    #         baseline_end_idx = int(self.amp_epoch[1]*sweep["sample_rate"])
-    #         baseline_Y = np.mean(sweep["dataY"][baseline_start_idx:baseline_end_idx])
-    #
-    #         amp_start_idx = int(self.amp_epoch[2]*sweep["sample_rate"])
-    #         amp_end_idx = int(self.amp_epoch[3]*sweep["sample_rate"])
-    #         amp_Y = np.mean(sweep["dataY"][amp_start_idx:amp_end_idx])
-    #
-    #         self._amp.append(amp_Y-baseline_Y)
-    #
-    #     return self._amp
-    #
-    # @property
-    # def slope(self):
-    #     if len(self.slope_epoch) != 2:
-    #         raise Exception("slope_epoch must be a list with two numbers")
-    #     elif self.slope_epoch[0] < 0:
-    #         raise Exception("slope_epoch[0] must be greater than zero")
-    #     self._slope = []
-    #     for sweep in self.sweeps:
-    #         if self.slope_epoch[1] > sweep["dataX"][-1]:
-    #             raise Exception("Vm_epoch[1] must be less than the length of the sweep")
-    #         start_idx = int(self.slope_epoch[0]*sweep["sample_rate"])
-    #         end_idx = int(self.slope_epoch[1]*sweep["sample_rate"])
-    #
-    #         rise = sweep["dataY"][end_idx] - sweep["dataY"][start_idx]
-    #         run = sweep["dataX"][end_idx] - sweep["dataX"][start_idx]
-    #
-    #         self._slope.append(rise/run)
-    #     return self._slope
 
     @property
     def dVdt(self):
