64fe479b18f48ed760cff8400a809e61377942d8
Perry Spratt <perrywespratt@gmail.com>
2020-01-17 21:50:19 -0800

    travis troubleshooting

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyibt',
+      version='0.0.1',
+      description='A python module for analyzing electrophysiological data',
+      author='Perry Sprat',
+      author_email='perrywespratt@gmail.com',
+      url='https://github.com/pspratt/pyibt',
+      packages=['pyibt'],
+     )
